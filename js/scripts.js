545af61fa6d1f36fa1515afccbab4c1385d8dfa0
Stephen Newkirk <newkirk771@gmail.com>
2016-08-26 10:22:12 -0700

    Make html form and output area.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
+//Back-End
 function Pizza(size, toppings) {
-  this.mass = size;
+  this.pSize= size;
   this.toppings = toppings;
 }
 
 Pizza.prototype.price = function() {
-  return (Math.round(this.mass * 1.3)) + (this.toppings * 2)
+  return (Math.round(this.pSize * 1.5)) + (this.toppings * 2);
 }
 
-var newPizza = new Pizza(2, 5);
-
-console.log(newPizza);
-var finalPrice = newPizza.price();
-console.log(finalPrice);
+//Front-End
