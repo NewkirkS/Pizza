a87eec5e735abf482ab7431c3ecc21f66dfb3c51
Stephen Newkirk <newkirk771@gmail.com>
2016-08-26 11:40:14 -0700

    Change toppings from value into array. Changed price method to calculate price of toppings based on length of array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 //Back-End
-function Pizza(size, toppings) {
+function Pizza(size) {
   this.pSize= size;
-  this.toppings = toppings;
+  this.toppings = [];
 }
 
 Pizza.prototype.price = function() {
-  return (Math.round(this.pSize * 1.5)) + (this.toppings * 2);
+  var sizePrice = (Math.round(this.pSize * 1.5));
+  var numOfToppings = this.toppings.length;
+  var totalPrice = sizePrice + (numOfToppings * 2);
+  return totalPrice;
 }
 
 //Front-End
+$(function(){
+
+  $("form").submit(function(event){
+    event.preventDefault();
+    var pizzaSize = $("select#pizza-size").val();
+    var newPizza = new Pizza(pizzaSize);
+  });
+});
